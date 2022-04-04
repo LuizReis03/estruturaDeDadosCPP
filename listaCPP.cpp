d8e50fbb01a7aea0ad5e03b6da6514df42398825
Luiz Reis <82242839+LuizReis03@users.noreply.github.com>
2022-04-04 20:33:07 -0300

    Update listaCPP.cpp

--- listaCPP.cpp
+++ listaCPP.cpp
@@ -5,44 +5,70 @@ using namespace std;
 
 int main(){
 	list<int> lista;
+	int opc = 0;
+	int capacidade = 0;
+	int valor = 0;
+	int primeiraPos = 0;
+	int segundaPos = 0;
 	
+	cout << "Digite o tamanho da lista: ";
+	cin >> capacidade;
+	capacidade+=1;
 	
 	list<int>::iterator it;
-	for(int i=0;i<5;i++){
+	for(int i=0;i<capacidade;i++){
 		lista.push_back(i);
 	}
 	
-	for(int i=5;i<10;i++){
-		lista.push_front(i);
-		
-	}
-    // Conteudo da lista: 9 8 7 6 5 0 1 2 3 4
-    cout << lista.front() << endl;
-    //printa 9 na tela
-    cout << lista.back() << endl;
-    // printa 4 na tela
-    cout << lista.size() << endl;
-    //printa o tamanho da lista na tela, que é igual a 10
-
-	for(it = lista.begin(); it!=lista.end();it++){
-		//printa os numeros pares começando do inicio da lista
-		if(*it %2 == 0){
-			cout << *it << endl;
+	int primeiroItem = lista.front();
+	int ultimoItem = lista.back();
+	
+	while (1){
+		cout << "\n1)Remover o primeiro item da lista\n";
+		cout << "2)Remover o ultimo item da lista\n";
+		cout << "3)Mostrar os valores da lista\n";
+		cout << "4)Adicionar valor na primeira posicao\n";
+		cout << "5)Sair\n";
+		cout << "\nDigite a opcao desejada: ";
+		cin >> opc;
+	
+		switch(opc){
+			
+			case 1: 
+				lista.remove(primeiroItem);
+				cout << "Removido com sucesso!!\n\n";
+				break;
+			
+			case 2:
+				lista.remove(ultimoItem);
+				cout << "Removido com sucesso!!\n\n";
+				break;
+			
+			case 3:
+				cout << "Digite o valor: ";
+				cin >> valor;
+				cout << "Inserir entre a posicao: ";
+				cin >> primeiraPos;
+				cout << "E a posicao: ";
+				cin >> segundaPos;
+				lista.insert(valor, primeiraPos, segundaPos);
+				break;
+				
+			case 4:
+				
+				break;
+			
+			case 5:
+				exit(0);
+				break;
+			
+			default:
+				cout << "\nAs opcoes sao apenas de 1 a 4!!\n\n";
+				break;
 		}
-	}
-    it--;
-    // adiciona dois 11 antes do 4 na lista
-    lista.insert(it,2,11);
-	for(it = lista.begin(); it!=lista.end();it++){
-		//printa os numeros pares
-		cout << *it << " ";
 	
 	}
-	cout << endl;
-	it--;//retira o 4 da lista
-	lista.erase(it);// printa o 3 que agora é o novo ultimo elemento da listacout << lista.back() << endl;
-    // limpa a lista inteira
-	lista.clear();
-    // printará 0 pois a lista está vazia
-	cout << lista.size() << endl;
+	
+	return 0;
 }
+

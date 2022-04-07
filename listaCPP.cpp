1e70685d3faf5f85bec06cf3a6592b41dfd970e8
Luiz Reis <82242839+LuizReis03@users.noreply.github.com>
2022-04-06 21:41:07 -0300

    Update listaCPP.cpp

--- listaCPP.cpp
+++ listaCPP.cpp
@@ -8,15 +8,18 @@ int main(){
 	int opc = 0;
 	int capacidade = 0;
 	int valor = 0;
+	int posicao = 0;
+	int itemLista = 0;
 	int qtde = 0;
 	
+	//loop para pegar capacidade da lista - so quebra se valor for maior que 0
 	while(1){
 		cout << "Digite o tamanho da lista: ";
 		cin >> capacidade;
 		capacidade-1;
 	
-		if(capacidade == 0){
-			cout << "Nao e possivel criar lista com capacidade de 0\n\n";
+		if(capacidade < 1){
+			cout << "Nao e possivel criar lista com capacidade de 0 ou menor que 0\n\n";
 			continue;
 		}else{
 			cout << "\nLista criada!\n";
@@ -33,11 +36,10 @@ int main(){
 	int ultimoItem = lista.back();
 	
 	while (1){
-		cout << "\n\n1)Remover o primeiro item da lista\n";
-		cout << "2)Remover o ultimo item da lista\n";
-		cout << "3)Adicionar valor no final da lista\n";
-		cout << "4)Mostrar os valores da lista\n";
-		cout << "5)Sair\n";
+		cout << "\n\n1)Remover um item da lista\n";
+		cout << "2)Adicionar valor no final da lista\n";
+		cout << "3)Mostrar os valores da lista\n";
+		cout << "4)Sair\n";
 		cout << "\nDigite a opcao desejada: ";
 		cin >> opc;
 	
@@ -45,17 +47,19 @@ int main(){
 			
 			case 1: 
 				//remove primeiro item
-				lista.remove(primeiroItem);
+				cout << "Qual a posicao do item que deseja remover: ";
+				cin >> posicao;
+				
+				for(int i=0;i<lista.size();i++){
+					if(i == posicao){
+						
+						lista.remove(posicao);
+					}
+				}				
 				cout << "Removido com sucesso!!\n\n";
 				break;
 			
 			case 2:
-				//remove ultimo item
-				lista.remove(ultimoItem);
-				cout << "Removido com sucesso!!\n\n";
-				break;
-			
-			case 3:
 				//insere valores
 				if (lista.size() < capacidade){
 					cout << "Digite a quantidade: ";
@@ -73,7 +77,7 @@ int main(){
 				}
 				break;
 				
-			case 4:
+			case 3:
 				//printa os valores da lista
 				cout << "\nValores da lista: ";
 				for(it = lista.begin(); it!=lista.end();it++){
@@ -82,7 +86,8 @@ int main(){
 	
 				break;
 			
-			case 5:
+			case 4:
+				//encerra o programa
 				exit(0);
 				break;
 			
